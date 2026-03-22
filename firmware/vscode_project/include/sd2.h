f5c3990bfd67621614ebede828049b5a2de85f0a
gregoryg03 <gglass4734@gmail.com>
2026-03-21 22:48:32 -0500

    Refactored SD cpp and h file. Ready to incorporate into other files

--- firmware/vscode_project/include/sd2.h
+++ firmware/vscode_project/include/sd2.h
@@ -2,32 +2,45 @@
 #define SD2_H
 
 #include <Arduino.h>
-#include "events.h"
 #include <SPI.h>
 #include <SD.h>
+#include "events.h"
 
-void sd_init(uint8_t);
-void sd_clear(void);
-void sd_write(uint16_t);
-bool sd_read(uint16_t &);
-void sd_close(void);
-void sd_read_init(void);
-void sd_start(void);
-void sd_end(void);
-
-extern File clockData;
-
-struct sdAction {
-    void(*clear)(void);
-    void(*write)(uint16_t);
-    bool(*read)(uint16_t &);
-    void(*dismount)(void);
+enum sdAction {
+    SD_OPEN,
+    SD_WRITE,
+    SD_READ,
+    SD_CLOSE
 };
 
-struct writeState {
-    void(*enter)();
-    void(*run)(uint16_t);
-    void(*exit)(void);
+enum sdMode {
+    READ_MODE = FILE_READ,
+    WRITE_MODE = FILE_WRITE
 };
+
+struct sdData {
+    sdMode mode;
+    uint16_t intervalIn;    //Interval being written to SD card
+    uint16_t intervalOut;   //Interval being read from SD card
+    bool success;           //Sucess flag for the action requested
+};
+
+using sdType = bool (*)(sdData&);
+
+class SD_CARD {
+    public:
+        void sd_init(uint8_t);
+        void sd_shutdown(void);
+
+        bool call(sdAction, sdData&);
+
+    private:
+        static sdType actions[4];
+};
+
+bool sd_open(sdData&);
+bool sd_write(sdData&);
+bool sd_read(sdData&);
+bool sd_close(sdData&);
 
 #endif
--- firmware/vscode_project/src/sd.cpp
+++ firmware/vscode_project/src/sd.cpp
@@ -1,20 +1,14 @@
 #include "sd2.h"
 
-const char *filename = "intervals.txt";
+const char *filename = "INTS.txt";
+
 File clockData;
-File readFile;
 
-struct sdAction sd = {
-    .clear = sd_clear,
-    .write = sd_write,
-    .read = sd_read,
-    .dismount = sd_close
+sdType SD_CARD::actions[4] = {
+    actions[0] = sd_open, actions[1] = sd_write,
+    actions[2] = sd_read, actions[3] = sd_close
 };
-//Use sd.clear, sd.write, sd.read
 
-writeState writeTable[] = {
-    {sd_start, sd_write, sd_end}
-};
 
 void sd_init(uint8_t CS_PIN)
 {
@@ -26,9 +20,59 @@ void sd_init(uint8_t CS_PIN)
     //Add Fail Message?
   }
   Serial.println("Init SD done.");
-  
 }
 
+
+bool sd_open(sdData& data)
+{
+    clockData = SD.open(filename, data.mode);
+        if (!clockData) {
+            Serial.println("SD Fail Start");
+            return false;
+        }
+    return true;
+}
+
+bool sd_write(sdData& data)
+{
+    if (clockData) {
+        Serial.print("Writing...");
+        clockData.write(reinterpret_cast<uint8_t*>(&data.intervalIn), sizeof(data.intervalIn));
+        return true;
+    }
+    else 
+        Serial.println("SD Fail Write");
+    return false;
+}
+
+bool sd_read(sdData& data)
+{
+    if (clockData.available() >= sizeof(data.intervalOut)) {
+        clockData.read(reinterpret_cast<uint8_t*>(&data.intervalOut), sizeof(data.intervalOut));
+        return true;
+    }
+    Serial.println("SD Read Fail");
+    return false;
+}
+
+
+bool sd_close(sdData& data)
+{
+    if (clockData) {
+        clockData.close();
+        Serial.println("close");
+        return true;
+    }
+    Serial.println("close Fail");
+    return false;
+}
+
+void sd_shutdown(void)
+{
+    if (clockData) {
+        clockData.close();
+    }
+}
 
 // void sd_clear(void)
 // {
@@ -37,48 +81,4 @@ void sd_init(uint8_t CS_PIN)
 //     }
 // }
 
-void sd_start(void)
-{
-    clockData = SD.open(filename, FILE_WRITE);
-    if (!clockData)
-        Serial.println("SD Fail Start");
-}
 
-void sd_write(uint16_t data)
-{   
-    if (clockData) {
-        clockData.println("Test Text");
-        clockData.write(reinterpret_cast<uint8_t*>(&data), sizeof(data));
-    }
-    else
-        Serial.println("SD Fail Write");
-}
-
-void sd_end(void)
-{
-    clockData.flush();
-    clockData.close();
-}
-
-void sd_read_init(void)
-{
-    readFile = SD.open(filename, FILE_READ);
-}
-
-bool sd_read(uint16_t &interval)
-{
-    if (!readFile)
-        return false;
-    
-    if (readFile.available() >= sizeof(interval)) {
-        readFile.read(reinterpret_cast<uint8_t*>(&interval), sizeof(interval));
-        return true;
-    }
-
-    return false;
-}
-
-void sd_close(void)
-{
-    clockData.close();
-}
