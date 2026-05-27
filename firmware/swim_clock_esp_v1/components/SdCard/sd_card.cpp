60692640fe7e45a3402a264fb0bd5c4fbbcc65d6
gregoryg03 <gglass4734@gmail.com>
2026-05-26 22:23:32 -0500

    Started sd integration

--- firmware/swim_clock_esp_v1/components/SdCard/sd_card.cpp
+++ firmware/swim_clock_esp_v1/components/SdCard/sd_card.cpp
@@ -2,47 +2,68 @@
 
 static const char *TAG = "sd_debug";
 
-//Testing intervals to cycle through (sec)
-const int timearr[10] = {20, 5, 20, 5, 30, 10, 60, 10, 20, 3000};
-int intevaltime = 0;
+typedef struct {
+    gpio_num_t mosi;
+    gpio_num_t miso;
+    gpio_num_t clock;
+    gpio_num_t cs;
+} sd_pins_t;
 
-// static sdData sdDataRead = {READ_MODE, 0, 0, false};
-// SD_CARD sdInstanceRead;
-//decimal Pt
-uint8_t dp = 0b00000000;
+sd_pins_t sd_pins;
 
-//Time
-unsigned long tcount;
-int shiftminsten = 0, shiftmins = 0, shiftsecsten = 0, shiftsecs = 0;
-int secs, secsten, mins, minsten;
-
-bool counting = false;
-bool flag = false;
-static int status;
-
-
-
-void test_sd(void)
+void sd_init(gpio_num_t mosi, gpio_num_t miso, gpio_num_t clock, gpio_num_t cs)
 {
-    ESP_LOGI(TAG, "SD Main");
+    esp_err_t ret;
 
-    static uint16_t curInt = 0;
-    static bool open_flag = false;
-    static int i = 0;
+    sd_pins.mosi = mosi;
+    sd_pins.miso = miso;
+    sd_pins.clock = clock;
 
-    if (!counting) {
-        curInt = timearr[i++];
-        status = curInt;
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+    };
+
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initilizing SD Card");
+
+    ESP_LOGI(TAG, "Using SPI Peripheral");
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = sd_pins.mosi,
+        .miso_io_num = sd_pins.miso,
+        .sclk_io_num = sd_pins.clock,
+        .quadhd_io_num = -1,
+        .quadwp_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initilize SPI bus.");
+        return;
     }
 
-    int minutes = curInt / 60, seconds = curInt % 60;
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = sd_pins.cs;
+    slot_config.host_id = host.slot;
 
-    shiftminsten = minutes / 10;
-    shiftmins = minutes % 10;
+    ESP_LOGI(TAG, "Mounting Filesystem");
 
-    shiftsecsten = seconds / 10;
-    shiftsecs = seconds % 10;
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem");
+        }
+        else {
+            ESP_LOGE(TAG, "Failed to initilize the card (%s).", esp_err_to_name(ret));
+        }
+        return;
+    }
 
-    counting = true;
-    flag = false;
+    ESP_LOGI(TAG, "Filesystem mounted");
+
 }
--- firmware/swim_clock_esp_v1/components/SdCard/sd_card.h
+++ firmware/swim_clock_esp_v1/components/SdCard/sd_card.h
@@ -1,12 +1,35 @@
 #ifndef SD_CARD_H
 #define SD_CARD_H
 
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
 #include <sys/stat.h>
+
+#include "driver/gpio.h"
+#include "driver/spi_common.h"
+#include "driver/sdspi_host.h"
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "freertos/FreeRTOS.h"
 #include "esp_log.h"
+
+#define MAX_CHAR_SIZE 64
+#define MOUNT_POINT "/sdcard"
+
 
 struct sdData {
     bool countFlag;
     uint8_t data[4]; 
 };
+
+//Function prototypes
+void sd_init(gpio_num_t, gpio_num_t, gpio_num_t);
+
+static esp_err_t write_file(const char *, char *);
+
+//edit this to take in a pointer for the output so it can be used in main
+static esp_err_t read_file(const char *);
+
 
 #endif
